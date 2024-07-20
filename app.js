5ace0fde8a54476a6db8994506a26fc4a37f7158
qkenn <github.com.sampling990@passinbox.com>
2024-07-20 15:14:46 +0530

    add input sanitze ability

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  console.log('form hooked');
+  const data = new FormData(e.target);
+
+  const title = DOMPurify.sanitize(data.get('book_title'));
+  const author = DOMPurify.sanitize(data.get('book_author'));
+  const pages = DOMPurify.sanitize(data.get('book_pages'));
+  const isRead = DOMPurify.sanitize(data.get('read_status'));
+
+  console.log(title, author, pages, isRead);
 });
