00c93c7f3d7b7e4fb731a9f88a7c6ec3d9663b3d
qkenn <github.com.sampling990@passinbox.com>
2024-07-20 16:19:47 +0530

    feat: add new book

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('form');
 
+const library = [];
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -10,5 +12,23 @@ form.addEventListener('submit', (e) => {
   const pages = DOMPurify.sanitize(data.get('book_pages'));
   const isRead = DOMPurify.sanitize(data.get('read_status'));
 
-  console.log(title, author, pages, isRead);
+  addToLibrary(title, author, +pages, Boolean(isRead));
+
+  console.log(library);
 });
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  library.push(newBook);
+}
+
+addToLibrary('Sapiens', 'Yuval Noah Harari', 498, true);
+addToLibrary('GenX', 'Batman', 200, false);
+addToLibrary('GenY', 'Spider-man', 300, true);
